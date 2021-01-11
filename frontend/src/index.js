cddfb665e404e26f5c30a59bbe4db31e93cb1f32
shmall27 <samuel27hall@gmail.com>
2021-01-10 20:36:44 -0600

    Starting on login/signup build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import UploadForm from './components/UploadForm';
 
 // Standard form upload
-ReactDOM.render(<UploadForm />, document.getElementById('upload-console'));
+// ReactDOM.render(<UploadForm />, document.getElementById('upload-console'));
