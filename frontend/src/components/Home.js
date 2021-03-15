64ef01f9b2b4f34b9161993b09c0b52b0079301e
shmall27 <samuel27hall@gmail.com>
2021-03-14 23:22:56 -0500

    Updated landing page with beta notes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,9 +4,18 @@ import SignUp from './SignUp';
 function Home() {
   return (
     <>
+      <h3>Welcome to the beta!</h3>
+      <p>Notes:</p>
+      <p>Browser Compatibility: Chrome, and Edge (that I know of)</p>
+      <p>Style: Coming soon...</p>
       <p>
-        LimboDrive allows you to share your files without having to store them
-        on 3rd party servers.
+        Passwords: As this is my very first web app, I would recommend not using
+        your banking password.
+      </p>
+      <p>
+        Incognito: I strongly recommend using an incognito window when using
+        this software. The File System Access API works much more seamlessly on
+        an incognito window (I have no idea why).
       </p>
       <Login />
       <br />
